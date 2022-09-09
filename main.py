b70f5cd2fbbd4d3b086c2ab7e8e2ea008fc87f36
akhilmedvolt <akhil@medvolt.ai>
2022-09-09 16:46:25 +0530

    new update

--- main.py
+++ main.py
@@ -5,38 +5,37 @@ from generate_3d_viz import generate_3d_viz
 from preprocessing import s3_download
 
 from pathlib import Path
-import os
 import shutil
 
 if __name__ == "__main__":
-    link = "s3://medvolt-drp/results/client_6/workspace_1/project_1/experiment_1/C1CCC(C(C1)CN2CCN(CC2)C3=NSC4=CC=CC=C43)CN5C(=O)C6C7CCC(C7)C6C5=O/"
-    download_folder = link.split("/")[8]
-    s3_download(link, download_folder)
+    link = "s3://medvolt-drp/results/client_6/workspace_1/project_1/experiment_1/C1CCC(C(C1)/"
+    s3_download(link)
 
-    f_list = [x for x in Path(download_folder).rglob('*.pdb')]
+    f_list = [x for x in Path("pdbid/").rglob('*.pdb')]
     f_list = [str(x) for x in f_list if "complex" in x.name]
     f_name = [x.split("/")[1] for x in f_list]
-    # print(f_list)  # Downloading complete
+    print(f_list)  # Downloading complete
     print("Downloaded Files")
 
     # Move the files to root dir
     for i in range(len(f_list)):
-        shutil.move(f_list[i], f"./{f_name[i]}.pdb")
+        shutil.copy(f_list[i], f"./{f_name[i]}")
 
     # List out moved files
     curr_files = [x.split("/")[1] for x in glob.glob("./*") if ".pdb" in x]
-    print(curr_files)
 
+    print("processing")
     for file in curr_files:
+        print("current file: ", str(file))
         try:
             generate_2d_viz(file)
         except:
             with open("log.txt", "a") as f:
-                f.write(f" 2D failed for {str(file)}")
+                f.write(f" 2D failed for {str(file)} \n")
             pass
         try:
             generate_3d_viz(file)
         except:
             with open("log.txt", "a") as f:
-                f.write(f" 3D failed for {str(file)}")
+                f.write(f" 3D failed for {str(file)} \n")
             pass
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,21 @@
 from cloudpathlib import CloudPath
 
 
-def s3_download(link, download_folder):
-    cp = CloudPath(link)
+def s3_download(link):
+    root = CloudPath(link)
     print("Downloading files")
-    cp.download_to(download_folder)
+    iter_ = 0
+    for f in root.rglob('/*.pdb'):
+        if "complex" in f.name:
+            filename = f.name.replace('/', '_')
+            print(f"{str(iter_)}:", filename)
+            f.download_to("./pdbid/" + filename)
+        else:
+            pass
+        iter_ += 1
 
-#
-# if __name__ == "__main__":
-#     link = "s3://medvolt-drp/results/client_6/workspace_1/project_1/experiment_1/C1CCC(C(C1)/"
-#     download_folder = link.split("/")[8]
-#     s3_download(link, download_folder,  )
+
+if __name__ == "__main__":
+    link = "s3://medvolt-drp/results/client_6/workspace_1/project_1/experiment_1/C1CCC(C(C1)/"
+    download_folder = link.split("/")[8]
+    s3_download(link)
